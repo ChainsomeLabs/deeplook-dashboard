7902b0d1af13ec064722b718ad983fabbcbee608
DaveVodrazka <dave.vodrazka@gmail.com>
2025-08-06 14:53:51 +0200

    fix: update ohlcv endpoint data parsing

--- src/candleChart/transform.ts
+++ src/candleChart/transform.ts
@@ -1,21 +1,17 @@
-import { formatPriceNum, formatSizeNum } from "./../common/utils";
 import type { CandlestickData, Time, UTCTimestamp } from "lightweight-charts";
-import type { CandleGraphData, OHLCV, Pool, VolumeData } from "../common/types";
+import type { CandleGraphData, OHLCV, VolumeData } from "../common/types";
 
-export const transform = (ohlcv: OHLCV[], pool: Pool): CandleGraphData => {
+export const transform = (ohlcv: OHLCV[]): CandleGraphData => {
   const res: CandleGraphData = ohlcv.reduce(
     (agg: CandleGraphData, v: OHLCV) => {
       const time = v.timestamp as UTCTimestamp;
       const ohlc: CandlestickData<Time> = {
+        ...v,
         time,
-        open: formatPriceNum(v.open, pool),
-        high: formatPriceNum(v.high, pool),
-        low: formatPriceNum(v.low, pool),
-        close: formatPriceNum(v.close, pool),
       };
       const volume: VolumeData = {
         time,
-        value: formatSizeNum(v.volume_base, pool),
+        value: Number(v.volume_base),
         color: v.close > v.open ? "#2ebd85" : "#f6465d",
       };
 
